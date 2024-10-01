e82645988db201aec5324dc995038e17f967b1dc
RippedKek <tanjeeb1909@gmail.com>
2024-10-02 01:56:39 +0600

    Implement Dashboard Appbar

--- bettersis/lib/screens/dashboard.dart
+++ bettersis/lib/screens/dashboard.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import '../utis/themes.dart'; 
+import '../utis/themes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login_page.dart';
 import 'lunchtoken.dart';
 
 class Dashboard extends StatefulWidget {
-  final Map<String, dynamic> userData; 
+  final Map<String, dynamic> userData;
 
   const Dashboard({super.key, required this.userData});
 
@@ -15,6 +15,7 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
   // Function to handle logout
   Future<void> _logout() async {
     await _auth.signOut();
@@ -41,9 +42,42 @@ class _DashboardState extends State<Dashboard> {
       data: theme,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Dashboard'),
+          toolbarHeight: 100,
           backgroundColor: theme.primaryColor,
-          shadowColor: theme.secondaryHeaderColor,
+          automaticallyImplyLeading: false, // To remove the back button
+          title: Padding(
+            padding: const EdgeInsets.only(top: 8.0), // Add padding here
+            child: Column(
+              children: [
+                // 'BetterSIS' Title with rounded borders
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Colors.white),
+                  ),
+                  child: const Text(
+                    'BetterSIS',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 4),
+                // 'Dashboard' title
+                const Text(
+                  'DASHBOARD',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          centerTitle: true, // Center the title in the AppBar
           actions: [
             IconButton(
               icon: const Icon(Icons.logout),
@@ -52,31 +86,81 @@ class _DashboardState extends State<Dashboard> {
             ),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Name: ${widget.userData['name']}", style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              Text("Email: ${widget.userData['email']}", style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              Text("Dept: ${widget.userData['dept']}", style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              Text("ID: ${widget.userData['id']}", style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              Text("Phone: ${widget.userData['phone']}", style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              Text("Section: ${widget.userData['section']}", style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              Text("Type: ${widget.userData['type']}", style: const TextStyle(fontSize: 18)),
-              IconButton(
-                icon: const Icon(Icons.qr_code),
-                tooltip: 'Generate Lunch Token QR',
-                onPressed: _navigateToLunchToken, 
+        body: Column(
+          children: [
+            Container(
+              color: theme.primaryColor,
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.black, 
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Welcome!',
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        ),
+                        Text(
+                          widget.userData['name'],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis, 
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          "ID: ${widget.userData['id']}",
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 13,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          "Department: ${widget.userData['dept'].toString().toUpperCase()}",
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 13,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          "Current Semester: ${widget.userData['semester']}",
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            // Generate QR button
+            ElevatedButton.icon(
+              onPressed: _navigateToLunchToken,
+              icon: const Icon(Icons.qr_code),
+              label: const Text('Generate Lunch Token', style: TextStyle(color: Colors.white),),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: theme.primaryColor, 
+                foregroundColor: Colors.white, 
+                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- bettersis/lib/screens/lunchtoken.dart
+++ bettersis/lib/screens/lunchtoken.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../modules/qr_code_widget.dart'; // Import the QrCodeWidget
+import '../modules/qr_code_widget.dart';
+import 'package:intl/intl.dart';
 
 class LunchToken extends StatelessWidget {
   final String userId;
@@ -8,6 +9,13 @@ class LunchToken extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Generate current timestamp and expiry date
+    final currentTimestamp = DateTime.now();
+    final expiryDate = currentTimestamp.add(const Duration(days: 1));
+    
+    // Format the data to be stored in the QR code
+    String qrData = '$userId\n${DateFormat('yyyy-MM-dd HH:mm:ss').format(currentTimestamp)}\n${DateFormat('yyyy-MM-dd HH:mm:ss').format(expiryDate)}';
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lunch Token'),
@@ -16,7 +24,7 @@ class LunchToken extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            QrCodeWidget(userId: userId), 
+            QrCodeWidget(qrData: qrData), 
             const SizedBox(height: 20), 
             ElevatedButton(
               onPressed: () {
