b37d02ff997164256e9bf5a10f0acb337a5acfab
Fahim Rahman <fahimrahman39281@gmail.com>
2024-10-13 23:22:03 +0600

    WebScrapping to get the intenet usage

--- internetUsage/automation/views.py
+++ internetUsage/automation/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .selenium_logic import login_and_get_usage
 
-# Create your views here.
+@api_view(['POST'])
+def get_usage(request):
+    print("Views module imported successfully.")
+
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    # username = "fahimrahman"
+    # password = "248"
+    
+    if not username or not password:
+        return Response({"error": "Missing username or password"}, status=400)
+    
+    try:
+        usage = login_and_get_usage(username, password)
+        print(usage)
+        return Response({"usage": usage})
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
