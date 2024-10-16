7115a4c5afe5d8fe5e7af41bc281deb6a93f7a6e
Fahim Rahman <fahimrahman39281@gmail.com>
2024-10-17 05:11:34 +0600

    upper part of internet screen

--- bettersis/lib/screens/Internet/internet_body.dart
+++ bettersis/lib/screens/Internet/internet_body.dart
@@ -27,19 +27,123 @@ class _InternetBodyState extends State<InternetBody> {
             padding: const EdgeInsets.all(8.0),
             decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(20.0)),
-            child: const Text(
+            child: Text(
               'IUT INTERNET',
               style: TextStyle(
-                fontSize: 14.0,
+                fontSize: 0.032 * screenSize.width,
                 fontWeight: FontWeight.w500,
-                color: Color(0xFF18A0D9),
+                color: theme.secondaryHeaderColor,
               ),
             ),
           ),
-          SizedBox(
-            width: screenSize.width,
-            height: screenSize.height * 0.01,
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 26.0),
+            decoration: const BoxDecoration(color: Colors.transparent),
+            child: const Column(
+              children: [
+                Text(
+                  'FAHIM RAHMAN',
+                  style: TextStyle(
+                    fontSize: 27.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '210041205',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 0.083 * screenSize.width),
+            padding: const EdgeInsets.only(top: 5.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.0),
+              boxShadow: [
+                BoxShadow(
+                  color:
+                      Colors.grey.withOpacity(0.5), // Shadow color with opacity
+                  spreadRadius: 5, // How much the shadow spreads
+                  blurRadius: 7, // How blurry the shadow is
+                  offset: const Offset(0,
+                      3), // The position of the shadow (horizontal, vertical)
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Minutes Used',
+                  style: TextStyle(
+                    fontSize: 0.037 * screenSize.width,
+                    fontWeight: FontWeight.w500,
+                    color: theme.secondaryHeaderColor,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 25,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '10,780',
+                        style: TextStyle(
+                          fontSize: 0.083 * screenSize.width,
+                          fontWeight: FontWeight.w700,
+                          color: theme.secondaryHeaderColor,
+                        ),
+                      ),
+                      Text(
+                        'out of',
+                        style: TextStyle(
+                          fontSize: 0.046 * screenSize.width,
+                          fontWeight: FontWeight.w700,
+                          color: theme.secondaryHeaderColor,
+                        ),
+                      ),
+                      Text(
+                        '12,000',
+                        style: TextStyle(
+                          fontSize: 0.083 * screenSize.width,
+                          fontWeight: FontWeight.w700,
+                          color: theme.secondaryHeaderColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+              padding: const EdgeInsets.only(top: 26),
+              decoration: const BoxDecoration(color: Colors.transparent),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.refresh,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "REFRESH",
+                    style: TextStyle(
+                      fontSize: 0.032 * screenSize.width,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
