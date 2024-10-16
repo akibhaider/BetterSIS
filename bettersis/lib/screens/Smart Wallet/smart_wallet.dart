2401d59843ba4bfde20304cc9053cad30f437466
Sameen Yeaser <142588275+sameenyeaser@users.noreply.github.com>
2024-10-17 04:17:34 +0600

    Implement firebase for finance and fetch data

--- bettersis/lib/screens/Smart Wallet/smart_wallet.dart
+++ bettersis/lib/screens/Smart Wallet/smart_wallet.dart
@@ -31,17 +31,48 @@ class smartWalletPage extends State<SmartWallet> {
   // Mock user data (will be replaced by Firebase data later)
 
   // Mock transaction data
-  final int totalTransactions = 4;
-  final List<Map<String, dynamic>> transactions = [
-    {"title": "North Cafeteria - Lunch", "type": "meal", "amount": 70.00},
-    {"title": "North Cafeteria - Breakfast", "type": "meal", "amount": 40.00},
-    {"title": "Bus: Uttara - IUT", "type": "bus", "amount": 30.00},
-    {"title": "North Cafeteria - Dinner", "type": "meal", "amount": 90.00},
-  ];
+  double balance = 0.0;
+  int totalTransactions = 0;
+  List<Map<String, dynamic>> transactions = [];
 
    //ThemeData theme = AppTheme.getTheme(widget.userData['dept']);
 
   //SmartWalletPage({super.key});
+
+  Future<void> fetchData() async {
+    //first balance is fetched.
+
+    try{
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('Finance')
+          .doc(widget.userId)
+          .get();
+      
+      if(userDoc.exists){
+        setState(() {
+          balance = userDoc['Balance'];
+          transactions = List<Map<String, dynamic>>.from(
+            userDoc['Transactions'] ?? [],
+          );
+          totalTransactions = transactions.length;
+
+          /*if(totalTransactions > 4){
+            transactions = transactions.sublist(totalTransactions - 4);
+          }*/
+        });
+      }
+    }
+
+    catch(error){
+      print("Error fetching data");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,22 +146,37 @@ class smartWalletPage extends State<SmartWallet> {
           ),
           const SizedBox(height: 10),
           // Add Money Button
-          ElevatedButton(
+          Container(
+            width: MediaQuery.of(context).size.width *0.8,
+            child: ElevatedButton(
             onPressed: () {},
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.white,
               foregroundColor: theme.secondaryHeaderColor,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              //maximumSize: const Size(150, 50),
             ),
-            child: Text(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/bKash.png',
+                  height: 24,
+                  width: 24,
+                ),
+                const SizedBox(width: 10),
+                Text(
               "Add Money",
               style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
                   color: theme.secondaryHeaderColor,
+              ),
             ),
-          ),
+            ],
+            ),
 
+          ),
           ),
 
           const SizedBox(height: 20),
@@ -192,7 +238,7 @@ class smartWalletPage extends State<SmartWallet> {
                       },
                     ),
                   ),
-                  TextButton(
+                  /*TextButton(
                     onPressed: () {
                       // Placeholder for "View More" action
                     },
@@ -200,7 +246,7 @@ class smartWalletPage extends State<SmartWallet> {
                       'View More >>',
                       style: TextStyle(color: Colors.blue),
                     ),
-                  ),
+                  ),*/
                 ],
               ),
             ),
@@ -210,148 +256,3 @@ class smartWalletPage extends State<SmartWallet> {
     );
   }
 }
-
-/*
-      crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Profile Section
-          /*const CircleAvatar(
-            radius: 40.0,
-            backgroundImage: NetworkImage(
-                'https://via.placeholder.com/150'), // Placeholder image
-          ),*/
-          const SizedBox(height: 10),
-          Text(
-            name,
-            style: const TextStyle(
-                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            studentId,
-            style: const TextStyle(color: Colors.white70, fontSize: 16),
-          ),
-          Text(
-            email,
-            style: const TextStyle(color: Colors.white70, fontSize: 16),
-          ),
-          const SizedBox(height: 20),
-          // Balance Section
-          Card(
-            color: Colors.white,
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  const Text(
-                    'BALANCE',
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    '৳${balance.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                        color: Color.fromARGB(255, 6, 55, 139),
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          // Add Money Button
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              foregroundColor: theme.secondaryHeaderColor,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-            ),
-            child: Text(
-              "Add Money",
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: theme.secondaryHeaderColor,
-            ),
-          ),
-
-          ),
-
-          const SizedBox(height: 20),
-
-          // Transactions Section
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'LATEST TRANSACTIONS',
-                    style: TextStyle(
-                        color: Colors.blue[900],
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 10),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: totalTransactions,
-                      itemBuilder: (context, index) {
-                        String type = transactions[index]['type'];
-                        return Card(
-                          margin: const EdgeInsets.symmetric(vertical: 8.0),
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.blue,
-                              child: Icon(
-                                type == 'meal'
-                                    ? Icons.restaurant_menu // Icon for meal
-                                    : Icons.directions_bus, // Icon for bus
-                                color: Colors.white,
-                              ),
-                            ),
-                            title: Text(transactions[index]['title']),
-                            subtitle: Text(type == 'meal'
-                                ? 'Meal Token'
-                                : 'Transportation'),
-                            trailing: Text(
-                              '৳${transactions[index]['amount'].toStringAsFixed(2)}',
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 16),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      // Placeholder for "View More" action
-                    },
-                    child: const Text(
-                      'View More >>',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-*/
