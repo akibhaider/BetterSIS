1358b64795c76f8659db79efa7089b8a5b001601
RippedKek <tanjeeb1909@gmail.com>
2024-11-14 04:17:20 +0600

    Admit card only take courses from enrolled courses

--- bettersis/lib/screens/Academics/Admit-Card/admit-card.dart
+++ bettersis/lib/screens/Academics/Admit-Card/admit-card.dart
@@ -2,8 +2,7 @@ import 'package:bettersis/modules/bettersis_appbar.dart';
 import 'package:bettersis/screens/Misc/appdrawer.dart';
 import 'package:bettersis/utils/themes.dart';
 import 'package:flutter/material.dart';
-import '../../../modules/Admit Card/generate_admit_card.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../../modules/Admit Card/generate-admit-card.dart';
 
 class AdmitCard extends StatefulWidget {
   final VoidCallback onLogout;
@@ -13,54 +12,21 @@ class AdmitCard extends StatefulWidget {
   final String userProgram;
   final String userSemester;
 
-  const AdmitCard(
-      {super.key,
-      required this.onLogout,
-      required this.userDept,
-      required this.userId,
-      required this.userName,
-      required this.userProgram,
-      required this.userSemester});
+  const AdmitCard({
+    super.key,
+    required this.onLogout,
+    required this.userDept,
+    required this.userId,
+    required this.userName,
+    required this.userProgram,
+    required this.userSemester,
+  });
 
   @override
   State<AdmitCard> createState() => _AdmitCardState();
 }
 
 class _AdmitCardState extends State<AdmitCard> {
-  Future<List<String>> _fetchAllTheoryCourses() async {
-    try {
-      // Fetch all course documents from the specified department and semester collections
-      QuerySnapshot courseSnapshot = await FirebaseFirestore.instance
-          .collection('Courses')
-          .doc(widget.userDept)
-          .collection(widget.userSemester[0])
-          .get();
-
-      // List to store theory course details as strings
-      List<String> theoryCoursesList = [];
-
-      for (var courseDoc in courseSnapshot.docs) {
-        String courseCode = courseDoc.id;
-
-        // Extract the last character and check if it's an odd number
-        int lastDigit = int.tryParse(courseCode[courseCode.length - 1]) ?? -1;
-
-        if (lastDigit % 2 == 1) {
-          Map<String, dynamic>? data =
-              courseDoc.data() as Map<String, dynamic>?;
-          String? courseName = data?['name'] ?? 'Unknown Course';
-          String courseString = '$courseCode: $courseName';
-          theoryCoursesList.add(courseString);
-        }
-      }
-
-      return theoryCoursesList;
-    } catch (error) {
-      print('Error fetching courses: $error');
-      return ['Error fetching courses'];
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     ThemeData theme = AppTheme.getTheme(widget.userDept);
@@ -68,32 +34,20 @@ class _AdmitCardState extends State<AdmitCard> {
     return Scaffold(
       drawer: CustomAppDrawer(theme: theme),
       appBar: BetterSISAppBar(
-          onLogout: widget.onLogout, theme: theme, title: "Admit Card"),
-      body: FutureBuilder<List<String>>(
-        future: _fetchAllTheoryCourses(),
-        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          if (snapshot.hasData && snapshot.data != null) {
-            return GenerateAdmitCard(
-              semester: (int.parse(widget.userSemester[0]) % 2 == 0)
-                  ? "Summer"
-                  : "Winter",
-              examination: "Mid",
-              registeredCourses: snapshot.data!,
-              userDept: widget.userDept,
-              userId: widget.userId,
-              userName: widget.userName,
-              userProgram: widget.userProgram,
-              userSemester: widget.userSemester,
-            );
-          }
-          return Center(child: Text('No courses available'));
-        },
+        onLogout: widget.onLogout, 
+        theme: theme, 
+        title: "Admit Card",
+      ),
+      body: GenerateAdmitCard(
+        semester: (int.parse(widget.userSemester[0]) % 2 == 0)
+            ? "Summer"
+            : "Winter",
+        examination: "Mid",
+        userDept: widget.userDept,
+        userId: widget.userId,
+        userName: widget.userName,
+        userProgram: widget.userProgram,
+        userSemester: widget.userSemester
       ),
     );
   }
