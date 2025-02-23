4af048fa0d37ae59b4b7f57de46df40ac3218a4a
RippedKek <tanjeeb1909@gmail.com>
2025-02-23 21:00:10 +0600

    minor changes

--- bettersis/lib/utils/bkash.dart
+++ bettersis/lib/utils/bkash.dart
@@ -1,4 +1,4 @@
-import 'package:bettersis/utils/bkash_helper.dart';
+//import 'package:bettersis/utils/bkash_helper.dart';
 import 'package:bkash/bkash.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
@@ -6,19 +6,23 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void bkashAddMoney(BuildContext context, double amount) async {
   try {
-    if (context == null) {
-      print("Error: Context is null");
-      return;
-    }
+    final bkash = Bkash(
+        bkashCredentials: BkashCredentials(
+          username: dotenv.env['bkash_username']!,
+          password: dotenv.env['bkash_password']!,
+          appKey: dotenv.env['bkash_api_key']!,
+          appSecret: dotenv.env['bkash_secret_key']!,
+        ),
+        logResponse: true);
 
     final response = await bkash.pay(
       context: context,
       amount: amount,
-      merchantInvoiceNumber: dotenv.env['invoice']!,
+      merchantInvoiceNumber: 'test12345',
     );
 
     print(response);
   } on BkashFailure catch (err) {
-    print(err.message);
+    print('bkash error: ' + err.message);
   }
 }
