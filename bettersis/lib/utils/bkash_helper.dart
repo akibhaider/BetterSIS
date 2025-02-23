ef4d0e2ee63b6e1b0cdd41e616877a00a39ebdb9
RippedKek <tanjeeb1909@gmail.com>
2025-02-23 16:14:12 +0600

    Use env for bkash secrets

--- bettersis/lib/utils/bkash_helper.dart
+++ bettersis/lib/utils/bkash_helper.dart
@@ -3,9 +3,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final bkash = Bkash(
     bkashCredentials: BkashCredentials(
-      username: 'sandboxTokenizedUser02',
-      password: 'sandboxTokenizedUser02@12345',
-      appKey: '4f6o0cjiki2rfm34kfdadl1eqq',
-      appSecret: '2is7hdktrekvrbljjh44ll3d9l1dtjo4pasmjvs5vl5qr3fug4b',
+      username: dotenv.env['bkash_username']!,
+      password: dotenv.env['bkash_password']!,
+      appKey: dotenv.env['bkash_api_key']!,
+      appSecret: dotenv.env['bkash_secret_key']!,
     ),
     logResponse: true);
